b15ff8349b2135fedaedadfdf810a821169414f3
rudra <rudreshtiwari57@gmail.com>
2023-07-24 11:28:24 +0530

    the browser opening and test

--- pysel/more_than_one_test.py
+++ pysel/more_than_one_test.py
@@ -6,17 +6,23 @@ import unittest
 from parameterized import parameterized
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+#the tests will run in the alphabetical order 
 class google(unittest.TestCase):
     def setUp(self):
         print("sample test case started")
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.google.com/")
-    @parameterized.expand(["javapoint","hello","java"])
-    def test_google(self,a):
-        self.driver.find_element(By.NAME, "q").send_keys(a)
-        self.driver.implicitly_wait(10)
-        WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.NAME,"btnK")))
+    def test_google(self):
+        self.driver.find_element(By.NAME, "q").send_keys("python")
+        time.sleep(2)
+        self.driver.find_element(By.NAME, "btnK").send_keys(Keys.ENTER)
+        time.sleep(3)
+
+    def test_CLASS(self):
+        self.driver.find_element(By.NAME, "q").send_keys("java")
+        time.sleep(2)
         self.driver.find_element(By.NAME, "btnK").send_keys(Keys.ENTER)
         time.sleep(3)
 
---
+++ pysel/openbrowsers.py
@@ -0,0 +1,33 @@
+#the opening of the browser is by creating object of of the chrome ,firefox,IE ect
+#sometimes you have to provide the path of the browser to the class while creating the object
+import time
+
+from selenium import webdriver
+import unittest
+from selenium.webdriver import Chrome, Keys
+from selenium.webdriver.common.by import By
+
+
+# we pass TestCase in the class to run the unittest
+class Openbrowser(unittest.TestCase):
+
+    #the setUp method is the first method to run will runing a test case
+    #there should always a setup method in the class
+    def setUp(self):
+        #creating the object of the chrome class
+        self.driver = Chrome()
+        self.driver.get("https://www.google.com/")
+    #the test definition name should always start with the "test_" then followed by the name of the test
+    def test_google(self):
+        self.driver.find_element(By.NAME, "q").send_keys("a")
+        time.sleep(2)
+        self.driver.find_element(By.NAME, "btnK").send_keys(Keys.ENTER)
+        time.sleep(2)
+    # this is the last method that will run after each test definition
+    def tearDown(self):
+
+        self.driver.quit()
+
+
+
+
